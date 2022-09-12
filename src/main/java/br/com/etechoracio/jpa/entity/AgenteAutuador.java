bbec75ab90eed9f0a6cb2f4ef8dc0ca187f6e548
Emilly.R-Henrique.N <email@test.com>
2022-09-12 10:48:58 -0300

    Criacao da entidade AgenteAutuador

---
+++ src/main/java/br/com/etechoracio/jpa/entity/AgenteAutuador.java
@@ -0,0 +1,37 @@
+package br.com.etechoracio.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import br.com.etechoracio.jpa.enums.StatusAgenteEnum;
+import br.com.etechoracio.jpa.enums.TipoAgenteEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_AGENTE_AUTUADOR")
+public class AgenteAutuador {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_AGENTE_AUTUADOR")
+	private Long id;
+	@Column(name = "TX_NOME")
+	private String nome;
+	@Column(name = "TX_FONE")
+	private String telefone;
+	@Column(name = "TP_AGENTE")
+	@Enumerated(EnumType.STRING)
+	private TipoAgenteEnum tipo;
+	@Column(name = "ST_AGENTE")
+	@Enumerated(EnumType.ORDINAL)
+	private StatusAgenteEnum  status;
+}
--- src/main/java/br/com/etechoracio/jpa/entity/Veiculo.java
+++ src/main/java/br/com/etechoracio/jpa/entity/Veiculo.java
@@ -1,4 +1,4 @@
-package entity;
+package br.com.etechoracio.jpa.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
